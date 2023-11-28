5be820ffff78f0c408c8299e1d9bad5c5f098ab8
Jason Lewis <jlewis@harperlove.com>
2023-11-28 14:10:56 -0500

    third commit

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,17 +1,31 @@
-// Header.js
+// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'; // Add styles as needed
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo">Your Company Logo</div>
-      <nav>
+    <header className="header">
+      <div className="logo-container">
+        <img src="/path/to/your/logo.png" alt="Company Logo" className="logo" />
+      </div>
+      <div className="company-info">
+        <h1>Trinity RealEstate Partners</h1>
+      </div>
+      <nav className="nav">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/realestate">Real Estate Development</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/realestate">Real Estate</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
         </ul>
       </nav>
     </header>
@@ -19,3 +33,4 @@ const Header = () => {
 };
 
 export default Header;
+
